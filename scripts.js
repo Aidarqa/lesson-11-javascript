13b7c954e2f319b30e288036f41bf4d8e410d65d
Aidar <108217893+Aidarqa@users.noreply.github.com>
2022-10-06 21:02:53 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,56 @@
+// var x = "Hello World! Codify";
+// let y = "Привет Мир!";
+// const a = "Это константа";
+// y = "Мы переопределили значение";
+// let i = "value"; 
+// let k = "value2"; 
+// let u = "value3";
+
+// // let word = "fasd";
+// // let firstWorld = "dvumya slovami";
+// // let ZAGLAVNYMI = 'ФЫВ'
+// // let API = "API";
+// // let api = "api";
+// let greet = "Hello World";
+
+// console.log(i);
+// console.log(x);
+// console.log(a);
+
+// // типы данных
+// // числа
+// let num = 31;
+// // строка
+// let num2 = "asdasdasfaskjdaslk21o^!&@^!&";
+// // логический тип данных (булеан)
+// let bool = true;
+// let bool2 = false;
+
+// console.log(num);
+// console.log(num2);
+
+
+// // Операторы
+// let num1 = 5;
+// let num3 = 4;
+// let sum = num1 + num3;
+// console.log(sum);
+
+// let string1 = "2";
+// let string2 = "1";
+// let number1 = 2;
+// let number2 = 1;
+
+// console.log(string1 + string2); //конкатенация у строк
+// console.log(string1 + number1); //JS не строго типизированный язык
+
+ // boolean
+let num1 = 10;
+let num2 = 20;
+let str1 = '10';
+let str2 = 10;
+
+console.log(str1 === str2);
+
+
+
